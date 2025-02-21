86b3fc63db09b4e1292cae897c29a214059cebe5
Josie Smith <jsmit194@syr.edu>
2025-02-21 18:01:38 -0500

    one_package complete

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,18 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+import streamlit as st
+from packaging import parse_packaging, calc_total_units, get_unit
+
+st.title('Process One Package')
+package = st.text_input('Enter package data')
+if package:
+    parsed = parse_packaging(package)
+    total_units = calc_total_units(parsed)
+    unit = get_unit(parsed)
+    st.text(parsed)
+    for item in parsed:
+        name = list(item.keys())[0]
+        size = list(item.values())[0]
+        st.info(f'{name} -> {size}')
+    st.success(f'Total: {total_units} {unit}')
